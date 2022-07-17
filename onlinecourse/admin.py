ede41fd58f649e8d7834ac36cdfe1a33aae00715
erikafvicente <erikaferreiravicente@gmail.com>
2022-07-17 10:58:39 +0200

    First Django app

---
+++ onlinecourse/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 3
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]#, QuestionInline, ChoiceInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines=[QuestionInline]
+    list_display = ['title']
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines=[ChoiceInline]
+    list_display = ['question']
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question)
+admin.site.register(Choice)
